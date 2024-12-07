ba3558c0b8611df5650ca2e1cb7de42c6856a31e
Ritesh <riteshbhoskar7@gmail.com>
2024-12-07 20:38:03 +0530

    debugging

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import IntroductionPage from "@/components/LandingPage";
-import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation";
-import { authOptions } from "@/lib/auth";
 
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
-  // if(session?.user){
-  //   const destination = session.user.role === "PATIENT" ? "/dashboard/patient" : "/dashboard/doctor"
-  //     redirect(destination);
-  // }
 
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  if (!session?.user) {
+    console.log("No session found, redirecting to login...");
+    redirect("/signin");
+  }
+  
+  if(session?.user){
+    const destination = session.user.role === "PATIENT" ? "/dashboard/patient" : "/dashboard/doctor";
+    console.log("Redirecting to:", destination);
+    redirect(destination);
+  }
   return (
     <main>
       {/* <IntroductionPage /> */}
