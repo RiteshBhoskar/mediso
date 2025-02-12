a7a002b032900968e01e950d6e497e9b70820669
RiteshBhoskar <riteshbhoskar7@gmail.com>
2025-02-12 22:20:08 +0530

    fixed the suspense error

--- components/ChatPage.tsx
+++ components/ChatPage.tsx
@@ -1,32 +1,45 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import ChatSection from "@/components/ChatSection";
 import { useSearchParams } from "next/navigation";
 import ConversationsSidebar from "./ConversationSidebar";
 
-const ChatPage = () => {
+function ChatPageLoading() {
+    return <div className="flex items-center justify-center h-full w-full">Loading Chat...</div>
+}
+
+const ChatPageContent = () => {
     const searchParams = useSearchParams();
     const appointmentId = searchParams.get('appointmentId');
 
     useEffect(() => {
-      const intervalId = setInterval(async () => {
-        try {
-            await fetch('/api/last-seen', { method: 'POST' });
-            console.log("Last seen updated");
-        } catch (error) {
-            console.error("Error updating last seen:", error);
-        }
-    }, 1500000);
+        const intervalId = setInterval(async () => {
+            try {
+                await fetch('/api/last-seen', { method: 'POST' });
+                console.log("Last seen updated");
+            } catch (error) {
+                console.error("Error updating last seen:", error);
+            }
+        }, 1500000);
 
-    return () => clearInterval(intervalId);
+        return () => clearInterval(intervalId);
     }, [])
 
     return (
         <div className="flex h-2/3 w-full">
-            <ConversationsSidebar /> 
+            <ConversationsSidebar />
             <ChatSection appointmentId={appointmentId} />
         </div>
+    );
+};
+
+
+const ChatPage = () => {
+    return (
+        <Suspense fallback={<ChatPageLoading />}>
+            <ChatPageContent />
+        </Suspense>
     );
 };
 
